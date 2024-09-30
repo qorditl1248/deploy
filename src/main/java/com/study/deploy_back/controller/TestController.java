ddf948cce947a22697e4fe8519bed883f4f83aed
qorditl1248 <qorditl1248@naver.com>
2024-09-30 15:17:33 +0900

    배포 연습중

--- src/main/java/com/study/deploy_back/controller/TestController.java
+++ src/main/java/com/study/deploy_back/controller/TestController.java
@@ -2,6 +2,7 @@ package com.study.deploy_back.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class TestController {
     @Value("${client.address}")
     private String clientAddress; // 나중에 배포되어지는 리액트 주소가 들어옴
 
+    @CrossOrigin(originPatterns = "*")
     @GetMapping("/test")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok(clientAddress + "테스트");
